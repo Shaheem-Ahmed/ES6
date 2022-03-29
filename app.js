4b927bc74395eeb222659e85dd68e3aa80930339
Shaheem-Ahmed <95481874+Shaheem-Ahmed@users.noreply.github.com>
2022-03-29 18:44:39 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,11 @@
-var arr = [
+var arr = 
     {
      FIRSTNAME:"Ahmed",
      LASTNAME:"Mohammed",
      group:"group"
-    },
-
-    {
-        FIRSTNAME:"Abdul Minhas",
-        LASTNAME:"Abid",
-        group:"group"
-    },
-
-    {
-        FIRSTNAME:"Areeb",
-        LASTNAME:"Wasim",
-        group:"group"
-    },
-
-    {
-        FIRSTNAME:"Ibad",
-        LASTNAME:"Younis",
-        group:"group"
-    },
-
-    {
-        FIRSTNAME:"Hanif",
-        LASTNAME:"Akram",
-        group:"group"
     }
- ];
 
+let arr1 = Object.keys(arr)
+let arr2 = Object.values(arr)
 
-var newArray = [];
-
-
-for (var i = 0; i < arr.length; i++){
-newArray.push(arr[i].LASTNAME);
-}
-
-for (var i = 0; i < arr.length; i++){
-newArray.push(arr[i].FIRSTNAME);
-}
-
-console.log(newArray.join(', '));
+console.log(arr1, arr2)
